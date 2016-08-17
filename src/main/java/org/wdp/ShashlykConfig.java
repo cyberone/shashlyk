e7e7abd31f38d9cee0ba45c50a4b730a8d7eddf3
Aleksey Popov <alopen@yandex.ru>
2016-08-17 22:49:46 +0700

    Added thymeleaf

---
+++ src/main/java/org/wdp/ShashlykConfig.java
@@ -0,0 +1,12 @@
+package org.wdp;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration.
+ * @author Aleksey Popov (alopen@yandex.ru)
+ */
+@Configuration
+public class ShashlykConfig extends WebMvcAutoConfiguration {
+}
