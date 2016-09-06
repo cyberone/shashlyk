2e8d8176cf6501635d55a29d24632071c71957f4
Aleksey Popov <alopen@yandex.ru>
2016-09-06 21:47:31 +0700

    Formatting

---
+++ src/main/java/org/wdp/shashlyk/classifier/DishClass.java
@@ -0,0 +1,12 @@
+package org.wdp.shashlyk.classifier;
+
+/**
+ * Dish class.
+ * @author Aleksey Popov (alopen@yandex.ru)
+ */
+public enum DishClass {
+    PLEBS,
+    PROLETARIAT,
+    BOYARS,
+    ;
+}
